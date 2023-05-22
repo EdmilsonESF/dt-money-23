43ef6744b8b908662a33c29eafc4a0282c0de71f
Edmilson Fonseca <ef.edmilsonfonseca@gmail.com>
2023-05-22 19:48:59 -0300

    implementa table de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,40 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { TransactionsContainer, TransactionsTable,PriceHighlight } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header/>
+      <Summary />
 
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tr>
+            <td width='50%'>Desenvolvimento</td>
+            <td>
+              <PriceHighlight variant="income">
+                R$ 12.000,00
+              </PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2023</td>
+          </tr>
+
+          <tr>
+            <td width='50%'>Hamburger</td>
+            <td>
+              <PriceHighlight variant="outcome">
+                - R$ 59,00
+              </PriceHighlight>
+            </td>
+            <td>Alimentação</td>
+            <td>10/04/2023</td>
+          </tr>
+        </TransactionsTable>
+      </TransactionsContainer>
+
+      
     </div>
   )
 }
